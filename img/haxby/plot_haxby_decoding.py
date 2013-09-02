0862ec82c44d78a3fdef4c3d76c01e89093bfbf8
Alexandre Abraham <abraham.alexandre@gmail.com>
2013-09-02 10:26:18 +0200

    Enhance Haxby and Kamitani figures
    Haxby: reduce size of figures, integrate title in the figure to save space
    Kamitani: zoom and voxel highlight in decoding setting

--- img/haxby/plot_haxby_decoding.py
+++ img/haxby/plot_haxby_decoding.py
@@ -1,7 +1,42 @@
+### Visualization function ####################################################
+import pylab as pl
+from matplotlib.patches import Rectangle
+
+
+def plot_haxby(activation, title):
+    z = 25
+
+    fig = pl.figure(figsize=(4, 5.4))
+    fig.subplots_adjust(bottom=0., top=1., left=0., right=1.)
+    pl.axis('off')
+    # pl.title('SVM vectors')
+    pl.imshow(np.rot90(mean_img[:, 4:58, z]), cmap=pl.cm.gray,
+              interpolation='nearest')
+    pl.imshow(np.rot90(activation[:, 4:58, z]), cmap=pl.cm.hot,
+              interpolation='nearest')
+
+    mask_house = nibabel.load(h.mask_house[0]).get_data()
+    mask_face = nibabel.load(h.mask_face[0]).get_data()
+
+    pl.contour(np.rot90(mask_house[:, 4:58, z].astype(np.bool)), contours=1,
+            antialiased=False, linewidths=4., levels=[0],
+            interpolation='nearest', colors=['blue'])
+
+    pl.contour(np.rot90(mask_face[:, 4:58, z].astype(np.bool)), contours=1,
+            antialiased=False, linewidths=4., levels=[0],
+            interpolation='nearest', colors=['limegreen'])
+
+    p_h = Rectangle((0, 0), 1, 1, fc="blue")
+    p_f = Rectangle((0, 0), 1, 1, fc="limegreen")
+    pl.legend([p_h, p_f], ["house", "face"])
+    pl.title(title, x=.05, ha='left', y=.90, color='w', size=28)
+
+
 ### Load Haxby dataset ########################################################
 from nilearn import datasets
 import numpy as np
 import nibabel
+
 dataset_files = datasets.fetch_haxby_simple()
 h = datasets.fetch_haxby()
 
@@ -20,29 +55,43 @@ mask = dataset_files.mask
 mean_img = fmri_data.mean(axis=-1)
 
 ### Restrict to faces and houses ##############################################
-
-# Keep only data corresponding to faces or houses
 condition_mask = np.logical_or(conditions == 'face', conditions == 'house')
 X = fmri_data[..., condition_mask]
 y = y[condition_mask]
 session = session[condition_mask]
 conditions = conditions[condition_mask]
 
-# We have 2 conditions
-n_conditions = np.size(np.unique(y))
-
-### Loading step ##############################################################
+### Masking step ##############################################################
 from nilearn.io import NiftiMasker
 from nibabel import Nifti1Image
 nifti_masker = NiftiMasker(mask=mask, sessions=session, smoothing_fwhm=4,
                            memory="nilearn_cache", memory_level=1)
-niimg = Nifti1Image(X, affine)
-X = nifti_masker.fit_transform(niimg)
+X_img = Nifti1Image(X, affine)
+X = nifti_masker.fit_transform(X_img)
 
-### Prediction function #######################################################
+###############################################################################
+#                                                                             #
+#   F-score                                                                   #
+#                                                                             #
+###############################################################################
 
-### Define the prediction function to be used.
-# Here we use a Support Vector Classification, with a linear kernel and C=1
+from sklearn.feature_selection import f_classif
+f_values, p_values = f_classif(X, y)
+p_values = -np.log10(p_values)
+p_values[np.isnan(p_values)] = 0
+p_values[p_values > 10] = 10
+p_unmasked = nifti_masker.inverse_transform(p_values).get_data()
+
+plot_haxby(p_unmasked, 'F-score')
+pl.savefig('haxby_fscore.pdf')
+pl.savefig('haxby_fscore.eps')
+
+###############################################################################
+#                                                                             #
+#   SVC                                                                       #
+#                                                                             #
+###############################################################################
+### Define the estimator
 from sklearn.svm import SVC
 clf = SVC(kernel='linear', C=0.01)
 
@@ -73,41 +122,63 @@ svc = clf.support_vectors_
 # reverse feature selection
 svc = feature_selection.inverse_transform(svc)
 # reverse masking
-niimg = nifti_masker.inverse_transform(svc[0])
+svc = nifti_masker.inverse_transform(svc[0])
 
 # We use a masked array so that the voxels at '-1' are displayed
 # transparently
-act = np.ma.masked_array(niimg.get_data(), niimg.get_data() == 0)
+act = np.ma.masked_array(svc.get_data(), svc.get_data() == 0)
 
-### Create the figure
-
-z = 25
-
-import pylab as pl
-from matplotlib.patches import Rectangle
-pl.figure(figsize=(5,7))
-pl.axis('off')
-# pl.title('SVM vectors')
-pl.imshow(np.rot90(mean_img[..., z]), cmap=pl.cm.gray,
-          interpolation='nearest')
-pl.imshow(np.rot90(act[..., z]), cmap=pl.cm.hot,
-          interpolation='nearest')
-
-mask_house = nibabel.load(h.mask_house[0]).get_data()
-mask_face = nibabel.load(h.mask_face[0]).get_data()
-
-pl.contour(np.rot90(mask_house[..., z].astype(np.bool)), contours=1,
-        antialiased=False, linewidths=4., levels=[0], interpolation='nearest',
-    colors=['blue'])
-
-pl.contour(np.rot90(mask_face[..., z].astype(np.bool)), contours=1,
-        antialiased=False, linewidths=4., levels=[0], interpolation='nearest',
-    colors=['limegreen'])
-
-p_h = Rectangle((0, 0), 1, 1, fc="blue")
-p_f = Rectangle((0, 0), 1, 1, fc="limegreen")
-pl.legend([p_h, p_f], ["house", "face"])
-pl.tight_layout()
-
+plot_haxby(act, 'SVC')
 pl.savefig('haxby_svm.pdf')
 pl.savefig('haxby_svm.eps')
+
+
+###############################################################################
+#                                                                             #
+#   Searchlight                                                               #
+#                                                                             #
+###############################################################################
+
+### Prepare masks #############################################################
+# - mask_img is the original mask
+# - process_mask_img is a subset of mask_img, it contains the voxels that
+#   should be processed (we only keep the slice z = 26 and the back of the
+#   brain to speed up computation)
+
+mask_img = nibabel.load(dataset_files.mask)
+
+# .astype() makes a copy.
+process_mask = mask_img.get_data().astype(np.int)
+process_mask[..., 26:] = 0
+process_mask[..., :24] = 0
+process_mask_img = nibabel.Nifti1Image(process_mask, mask_img.get_affine())
+
+# Make processing parallel
+# /!\ As each thread will print its progress, n_jobs > 1 could mess up the
+#     information output.
+n_jobs = 1
+
+### Define the score function used to evaluate classifiers
+# Here we use precision which measures proportion of true positives among
+# all positives results for one class.
+from sklearn.metrics import precision_score
+score_func = precision_score
+
+### Define the cross-validation scheme used for validation.
+# Here we use a KFold cross-validation on the session, which corresponds to
+# splitting the samples in 4 folds and make 4 runs using each fold as a test
+# set once and the others as learning sets
+from sklearn.cross_validation import KFold
+cv = KFold(y.size, k=4)
+
+import nilearn.decoding
+# The radius is the one of the Searchlight sphere that will scan the volume
+searchlight = nilearn.decoding.SearchLight(mask_img,
+                                      process_mask_img=process_mask_img,
+                                      radius=5.6, n_jobs=n_jobs,
+                                      score_func=score_func, verbose=1, cv=cv)
+searchlight.fit(X_img, y)
+s_scores = np.ma.array(searchlight.scores_, mask=np.logical_not(process_mask))
+plot_haxby(s_scores, 'Searchlight')
+pl.savefig('haxby_searchlight.pdf')
+pl.savefig('haxby_searchlight.eps')
